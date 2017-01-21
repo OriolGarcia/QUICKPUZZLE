9dfb645beecb6a88f3b32e1d9406bd6b1bac01b8
OriolGarcia <130761.clot@fje.edu>
2017-01-21 12:34:34 +0100

    20170121Drag&drop Image drag

--- src/main/java/com/dam2/android/quickpuzzle/GameFragment.java
+++ src/main/java/com/dam2/android/quickpuzzle/GameFragment.java
@@ -1,12 +1,17 @@
 package com.dam2.android.quickpuzzle;
 
+import android.content.ClipData;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class GameFragment extends Fragment {
 
@@ -17,6 +22,30 @@ public static GameFragment newInstance(){
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.activity_game_fragment,container,false);
+
+        ImageView imgView=(ImageView) v.findViewById(R.id.iVcasella);
+
+                imgView.setImageResource(R.drawable.peca);
+           imgView.setOnTouchListener(new View.OnTouchListener()
+            {
+
+                @Override
+                public boolean onTouch(View view, MotionEvent motionEvent)
+                {
+                    if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                        ClipData data = ClipData.newPlainText("", "");
+                        View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(view);
+                        view.startDrag(data, shadowBuilder, view, 0);
+                        view.setVisibility(View.INVISIBLE);
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
+            });
+
+
         return v;
     }
+
 }
