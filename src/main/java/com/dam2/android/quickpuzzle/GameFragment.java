b70a4939928dd22b73173c4bb8810e7ada4eba6a
OriolGarcia <130761.clot@fje.edu>
2017-01-22 13:11:29 +0100

    20170121Drag&drop grid drag drop amb fragments començat

--- src/main/java/com/dam2/android/quickpuzzle/GameFragment.java
+++ src/main/java/com/dam2/android/quickpuzzle/GameFragment.java
@@ -1,11 +1,13 @@
 package com.dam2.android.quickpuzzle;
 
 import android.content.ClipData;
+import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -39,15 +41,106 @@ public static GameFragment newInstance(){
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.activity_game_fragment,container,false);
+        View v = inflater.inflate(R.layout.fragment_quickpuzzle,container,false);
 
-        GridView gridView = (GridView) v.findViewById(R.id.grid_view);
+        RecyclerView recyclerView = (RecyclerView) v.findViewById(R.id.fragment_quickpuzzle);
 
         // Instance of ImageAdapter Class
-        gridView.setAdapter(new ImageAdapter(v.getContext(),mThumbIds));
+        recyclerView.setLayoutManager(new GridLayoutManager(getActivity(),3));
+        recyclerView.setAdapter( new ImageAdapter2(mThumbIds) );
 
         return v;
     }
 
 
+
+
+    private class PecaHolder extends RecyclerView.ViewHolder{
+
+        private ImageView mImage;
+        public PecaHolder(LayoutInflater inflater, ViewGroup container){
+
+            super(inflater.inflate(R.layout.list_item, container,false));
+            mImage=(ImageView) itemView.findViewById( R.id.list_item_image);
+            container.setOnDragListener(new MyDragListener());
+        }
+    }
+
+        private class ImageAdapter2 extends RecyclerView.Adapter<PecaHolder>{
+            private Integer[] mThumbIds;
+           // private Context context;
+
+            public ImageAdapter2(Integer[] mThumbIds) {
+                this.mThumbIds=mThumbIds;
+                //this.context = context;
+            }
+            @Override
+            public PecaHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                LayoutInflater inflater = LayoutInflater.from(getActivity() );
+                return new PecaHolder( inflater,parent );
+            }
+
+            @Override
+            public void onBindViewHolder(PecaHolder holder, int position) {
+                holder.mImage.setImageResource( mThumbIds[position] );
+                holder.mImage.setOnTouchListener(new MyTouchListener());
+
+            }
+
+            @Override
+            public int getItemCount() {
+                return mThumbIds.length;
+            }
+        }
+}
+class MyTouchListener implements View.OnTouchListener {
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+            ClipData data = ClipData.newPlainText("", "");
+            View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(
+                    view);
+            view.startDrag(data, shadowBuilder, view, 0);
+            view.setVisibility(View.INVISIBLE);
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+class MyDragListener implements View.OnDragListener {
+    Context context;
+    //Drawable enterShape = context.getResources().getDrawable(R.drawable.shape2);
+    //  Drawable normalShape = context.getResources().getDrawable(R.drawable.shape);
+//MyDragListener(Context context){this.context=context;}
+    @Override
+    public boolean onDrag(View v, DragEvent event) {
+        int action = event.getAction();
+        switch (event.getAction()) {
+            case DragEvent.ACTION_DRAG_STARTED:
+                // do nothing
+                break;
+            case DragEvent.ACTION_DRAG_ENTERED:
+                //  v.setBackgroundDrawable(enterShape);
+                break;
+            case DragEvent.ACTION_DRAG_EXITED:
+                //  v.setBackgroundDrawable(normalShape);
+                break;
+            case DragEvent.ACTION_DROP:
+                // Dropped, reassign View to ViewGroup
+                v.setX(event.getX());
+                v.setY(event.getY());
+               /* View view = (View) event.getLocalState();
+                ViewGroup owner = (ViewGroup) view.getParent();
+                owner.removeView(view);
+                LinearLayout container = (LinearLayout) v;
+                container.addView(view);
+                view.setVisibility(View.VISIBLE);*/
+                break;
+            case DragEvent.ACTION_DRAG_ENDED:
+                //  v.setBackgroundDrawable(normalShape);
+            default:
+                break;
+        }
+        return true;
+    }
 }
